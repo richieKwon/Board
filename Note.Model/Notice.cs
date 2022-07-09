f64f4e0255da4f8d6da79b7891d1f619bcbafe40
richie <richie.kwon1@gmail.com>
2022-07-09 16:21:49 +0100

    Initial commit

---
+++ Note.Model/Notice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Note.Model
+{
+    public class Notice
+    {
+        [Key] public int NoticeNo { get; set; }
+
+        [Required] public string NoticeTitle { get; set; }
+
+        [Required] public string NoticeContents { get; set; }
+
+        [Required] public DateTime NoticePostingDate { get; set; }
+    }
+}
