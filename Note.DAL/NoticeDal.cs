02bb5c0488a538be535cd8ebf0502d582b03b1c4
richie <richie.kwon1@gmail.com>
2022-07-09 18:24:31 +0100

    DataAccess layer

---
+++ Note.DAL/NoticeDal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Note.IDAL;
+using Note.Model;
+
+namespace Note.DAL
+{
+    public class NoticeDal : INoticeDal
+    {
+        public List<Notice> GetNoticeList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Notice GetNotice(int noticeNo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PostNotice(Notice notice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateNotice(Notice notice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteNotice(int noticeNo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Note.IDAL/INoticeDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Note.Model;
+
+namespace Note.IDAL
+{
+    public interface INoticeDal
+    {
+        List<Notice> GetNoticeList();
+
+        Notice GetNotice(int noticeNo);
+
+        bool PostNotice(Notice notice);
+
+        bool UpdateNotice(Notice notice);
+
+        bool DeleteNotice(int noticeNo);
+    }
+}
